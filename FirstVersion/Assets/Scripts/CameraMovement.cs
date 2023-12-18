17bfa23e693c66c3f4ec889c369bddf866b99691
petritg <petrit.gashi@student.ehb.be>
2023-12-18 12:55:14 +0100

    Mobs v1
    ffff

---
+++ FirstVersion/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:32986e5bd4a40042abceaa82ee87e5c2f4e4c7a0c148eb48ffecabecd74ec51d
+size 332
---
+++ FirstVersion/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7c0e45760014a73c0d7e0bacee35a4a719ec864b21d4230ac5d8722c912d6bed
+size 570
