97a515a689435458fbbb0d70fe292428fe99e016
RobbePoedts <133092245+Poedtske@users.noreply.github.com>
2023-12-21 10:39:02 +0100

    can now go to endlessmode and storymode from mainmenu
    fixed the problem stopping this halting this
    Also fixed layer issue with player and testSpell in schoolmap

--- FirstVersion/Assets/Scripts/MenuController.cs
+++ FirstVersion/Assets/Scripts/MenuController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83c9e40911d94df36c8a2f15575ca7410f2ff49edb8739b867c9b758932f86ca
-size 8255
+oid sha256:35be85e54394396097b07d6ec0727558d59ccb69906f29fec602bfde75a5104d
+size 8291
