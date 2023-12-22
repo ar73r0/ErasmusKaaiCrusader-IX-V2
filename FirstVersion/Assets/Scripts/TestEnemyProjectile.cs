ea51242f1b45250d1c21468c8af95d220d570992
RobbePoedts <133092245+Poedtske@users.noreply.github.com>
2023-12-22 19:35:48 +0100

    Shooting enemy
    Enemy can now fire and kill player

---
+++ FirstVersion/Assets/Scripts/TestEnemyProjectile.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aeaa56201564392bf4a17321ad70c7203de3295fc6a72da1b8ae345da3b3e40a
+size 565
---
+++ FirstVersion/Assets/Scripts/TestEnemyShooting.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:24bdde8d46cd55e84ffc363fe0b5e065aeaff1b7439e33e6a5ed05cf53ff01c8
+size 1123
