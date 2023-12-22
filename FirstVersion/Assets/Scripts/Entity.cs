d4d760f0dc230ab089c27fca134c46346cf4b49c
jurgen <133093619+Jpjoker@users.noreply.github.com>
2023-12-22 22:57:14 +0100

    MapControllen Error fixed

--- FirstVersion/Assets/Scripts/Entity.cs
+++ FirstVersion/Assets/Scripts/Entity.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd1252ff2dd8b87399fceb7b9795e355740ad50e287cdec8b9829a18d8e8d1fe
-size 1455
+oid sha256:410054b407e84f526a7aaebb8e4f8862c9045e9e88e08bfc50b0b89b0ff44432
+size 1538
--- FirstVersion/Assets/Scripts/Player/PlayerMovement.cs
+++ FirstVersion/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d98e84dcc26e36e468344f52bc3b9ef98610c4208a37ba7b30dcd86aecd34923
-size 2581
+oid sha256:81bc4b26e5673edc4e833a90b64d3bd4060bdeface381528a8e514a84720f6f2
+size 2589
