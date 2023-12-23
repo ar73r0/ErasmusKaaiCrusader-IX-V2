a6dd3f8430748362fe319fd710153f7a4b997fcc
RobbePoedts <133092245+Poedtske@users.noreply.github.com>
2023-12-23 20:01:08 +0100

    fixed Enemy hitbox and UI
    Enemy orge now has a hitbox and can show UI

--- FirstVersion/Assets/Scripts/EnemyReceiveDamage.cs
+++ FirstVersion/Assets/Scripts/EnemyReceiveDamage.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aad190302594ac22aa9a3287db038d879b31a9ca515e2e2808604ba343cd005b
-size 1137
+oid sha256:f4d4bd814c94acd3045534c2012cb5d2208e1fd10ab4e76a008667ab59a5ac7b
+size 1121
--- FirstVersion/Assets/Scripts/TestEnemyShooting.cs
+++ FirstVersion/Assets/Scripts/TestEnemyShooting.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a3a123fa844b3c458668f0b3b440ebaa235f86f7f6a3d8858ae09c68c578372
-size 1193
+oid sha256:091173651c0b51c134d7577c4b59cea4879ce8f49e56fb3ef419431c0ecd463d
+size 1338
