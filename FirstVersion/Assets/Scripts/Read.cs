1905de8e47eb163de627ee47db8df155d1f69ffa
jurgen <133093619+Jpjoker@users.noreply.github.com>
2023-12-24 14:03:24 +0100

    Player Object postion
    SQL connection commend and player in the right position

---
+++ FirstVersion/Assets/Scripts/Read.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5d92fd5b7813679b49c63bd985010958ad123a4b56e8b579e0cdd75654f85658
+size 306
---
+++ FirstVersion/Assets/Scripts/Write.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c3612b37ad58bfe141e43dccb43564f20a02063acfd76964833d4e3874bc0c03
+size 858
